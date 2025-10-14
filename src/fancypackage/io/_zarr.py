d8c1b41ab139101633ed15f76aef9d40db4098db
Philipp Weiler <weiler.philipp@gmail.com>
2025-10-14 09:08:05 -0400

    Add `read_zarr`

--- src/fancypackage/io/_zarr.py
+++ src/fancypackage/io/_zarr.py
@@ -3,6 +3,7 @@ from pathlib import Path
 
 import zarr
 
+import anndata as ad
 from anndata import AnnData
 
 
@@ -25,3 +26,22 @@ def write_zarr(adata: AnnData, path: Path) -> None:
         store = zarr.storage.ZipStore(path, mode="w")
         adata.write_zarr(store=store)
         store.close()
+
+
+def read_zarr(path: Path) -> AnnData:
+    """Read hierarchical Zarr array zip store to AnnData.
+
+    Parameters
+    ----------
+    path
+        Filename of Zarr store.
+
+    Returns
+    -------
+    Read AnnData object.
+    """
+    store = zarr.storage.ZipStore(path, mode="r")
+    adata = ad.io.read_zarr(store)
+    store.close()
+
+    return adata
