c930388e65675b8b016ff60e52db4b96bf7816d8
Philipp Weiler <weiler.philipp@gmail.com>
2026-09-08 16:46:44 -0400

    fix: support https and ssh for gh remote

--- {{ cookiecutter.project_name }}/.set_gh_remote.sh
+++ {{ cookiecutter.project_name }}/.set_gh_remote.sh
@@ -3,4 +3,8 @@ set -e
 
 source ./.gh_auth.sh
 git remote remove origin 2>/dev/null || true
-git remote add origin "https://github.com/{{ cookiecutter.github_namespace }}/{{ cookiecutter.github_repo_name }}.git"
+if [ "$(gh config get git_protocol --host github.com)" = "ssh" ]; then
+  git remote add origin "git@github.com:{{ cookiecutter.github_namespace }}/{{ cookiecutter.github_repo_name }}.git"
+else
+  git remote add origin "https://github.com/{{ cookiecutter.github_namespace }}/{{ cookiecutter.github_repo_name }}.git"
+fi
